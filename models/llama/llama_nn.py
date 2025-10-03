860474ec5691bda98f8a79fe427bad90ae2228a3
Yuntong Li <aerith@Yuntongs-MacBook-Pro.local>
2025-10-03 21:03:39 +0800

    update

--- models/llama/llama_nn.py
+++ models/llama/llama_nn.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributed.pipelining import pipe_split
+import torch.distributed as dist
 
 @dataclass
 class LlamaConfig:
