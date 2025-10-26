77ff2795d76d14b7f7d1abd4ec20a45939d8e151
Yuntong Li <aerith@Yuntongs-MacBook-Pro.local>
2025-10-26 16:23:52 +0800

    update

---
+++ scripts/1dp_4pp/0_train_llama7b_standard.sh
@@ -0,0 +1,13 @@
+NCCL_IB_DISABLE=1 NCCL_SOCKET_IFNAME=bond0 \
+torchrun --nproc_per_node=2 --nnodes=2 --node_rank=0 --master_addr=10.82.252.20 --master_port=11234 \
+tests/train_llama7b_polar_dp_pp.py                  \
+--pp_size=4                                         \
+--epochs 1                                          \
+--batch_size 16                                     \
+--seq_length 512                                    \
+--lr 1e-4                                           \
+--dataset wikitext                                  \
+--dataset_config wikitext-2-raw-v1                  \
+--tokenizer hf-internal-testing/llama-tokenizer     \
+--output_dir ./checkpoints                          \
+--micro_batches 8                                   \
