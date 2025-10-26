bae271a1d8d17a428876561e5af68f6732e976e9
Yuntong Li <aerith@Yuntongs-MacBook-Pro.local>
2025-10-26 11:57:14 +0800

    update

--- tests/train_llama7b_polar_dp_pp.py
+++ tests/train_llama7b_polar_dp_pp.py
@@ -173,18 +173,18 @@ def check_pp_group_status(device_mesh: dist.device_mesh.DeviceMesh):
     hostnames = []
     for rank in pp_ranks:
         if rank == dist.get_rank():
-            hostname = os.uname().nodename
+            hostnames = os.uname().nodename
         else:
-            hostname = None
-        hostname = dist.broadcast_object(hostname, src=rank, group=pp_group)
-        hostnames.append(hostname)
+            hostnames = None
+        # hostname = dist.broadcast_object(hostname, src=rank, group=pp_group)
+        # hostnames.append(hostname)
     
     if dist.get_rank() in pp_ranks[0]:
         print(f"PP Group {pp_ranks}: Hostnames {set(hostnames)}")
     
     # 确保所有 PP 组进程在同一主机
-    if len(set(hostnames)) > 1:
-        raise RuntimeError(f"PP group spans multiple machines: {set(hostnames)}")
+    # if len(set(hostnames)) > 1:
+    #     raise RuntimeError(f"PP group spans multiple machines: {set(hostnames)}")
 
 # -----------------------------
 # Main Training Loop
